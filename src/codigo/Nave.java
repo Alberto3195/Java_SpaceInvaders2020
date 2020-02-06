3ee54ab92427ac891b741be99f875a009d54b9ef
Alberto <Alberto@LAPTOP-RD4DMT3L>
2020-02-06 16:45:18 +0100

    Añadida la nave (no se mueve)

---
+++ src/codigo/Nave.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Alberto Sanz
+ */
+public class Nave {
+
+    Image imagen = null;
+    int posX = 0;
+    int posY = 0;
+    private boolean derecha = false;
+    private boolean izquierda = false;
+
+    public Nave() { //el constructor es el metodo de igual nombre que la clase
+        //pero no devuelve nada por ello es public asecas
+        try {//Siempre que se hace una lectura con algo que hay en el disco, se 
+            //ejecuta un try, catch, esto hace que proteja lo que se encuentra
+            //en el disco.
+            imagen = ImageIO.read(getClass().getResource("/imagenes/nave.png"));
+        } //En caso de no poner IO en el catch se transforma en un Exception generico
+        //con errores genericos
+        catch (Exception e) {
+
+        }
+    }
+    
+    public void mueve(){ //Chequea las variables que tiene internas
+        if (izquierda){
+            posX--;
+        }
+        if(derecha){
+            posX++;
+        }
+    }
+}
